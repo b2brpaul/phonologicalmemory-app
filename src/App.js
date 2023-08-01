702fa782e1f0abb13b2b09a8725e8883c0e942b7
Votre Nom <oldmacbookbro@macbookbro.home>
2023-08-01 23:55:51 +0200

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Confetti from 'react-dom-confetti';
 import Modal from 'react-modal';
@@ -16,6 +16,7 @@ function App() {
   const [alertMessage, setAlertMessage] = useState("");
   const [reversedSequence, setReversedSequence] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const sequenceRef = useRef(null);
   const confettiConfig = {
     angle: 90,
     spread: 360,
@@ -35,30 +36,37 @@ function App() {
     setDigitSequence(sequence);
     setReversedSequence([...sequence].reverse());
     setCountdown(timerLength);
+
+    setTimeout(() => {
+    if (sequenceRef.current) {
+      sequenceRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+    }, 100);  // Delay of 100 milliseconds
   }
 
-useEffect(() => {
-  if (digitSequence.length > 0) {
-    setCountdown(timerLength);
-    const timer = setTimeout(() => {
-      setDigitSequence([]);
-      setShowSubmit(true);
-    }, timerLength * 1000);
-    const countdownTimer = setInterval(() => {
-      setCountdown(prevCountdown => prevCountdown > 0 ? prevCountdown - 1 : 0);
-    }, 1000);
-    return () => {
-      clearTimeout(timer);
-      clearInterval(countdownTimer);
-    };
-  }
-}, [digitSequence]);
+  useEffect(() => {
+    if (digitSequence.length > 0) {
+      setCountdown(timerLength);
+  
+      const timer = setTimeout(() => {
+        setDigitSequence([]);
+        setShowSubmit(true);
+      }, timerLength * 1000);
+      const countdownTimer = setInterval(() => {
+        setCountdown(prevCountdown => prevCountdown > 0 ? prevCountdown - 1 : 0);
+      }, 1000);
+      return () => {
+        clearTimeout(timer);
+        clearInterval(countdownTimer);
+      };
+    }
+  }, [digitSequence]);
 
-useEffect(() => {
-  if (countdown === 0 && submitRef.current) {
-    submitRef.current.scrollIntoView({ behavior: 'smooth' });
-  }
-}, [countdown]);
+  useEffect(() => {
+    if (countdown === 0 && submitRef.current) {
+      submitRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [countdown]);
 
   function checkSequence() {
     if (userSequence.join(' ') !== reversedSequence.join(' ')) {
@@ -79,7 +87,7 @@ useEffect(() => {
     setUserSequence([]);
     setAlertMessage("");
     setReversedSequence([]);
-    setShowModal(false);  // Add this line
+    setShowModal(false);
   }
 
   function handleClick(num) {
@@ -91,39 +99,43 @@ useEffect(() => {
   return (
     <div className="App">
       <h1>Phonologic Memory Game</h1>
-      <h2>{name}, your score: {score}</h2>
+      <h2>{name ? `${name}, your score: ${score}` : `Your score: ${score}`}</h2>
       <div className="confetti-container">
-      <Confetti active={alertMessage === 'Correct!'} config={confettiConfig} />
+        <Confetti active={alertMessage === 'Correct!'} config={confettiConfig} />
       </div>
-    <hr />
-      <p> Let's start by configuring the game :</p> 
-<label>
-  Name: 
-  <input type="text" value={name} onChange={e => setName(e.target.value)} />
-</label>
-      <p>I want the sequence to be :&nbsp;
-        <select value={numDigits} onChange={e => setNumDigits(Number(e.target.value))}>
-          {Array.from({ length: 16 }, (_, i) => i + 5).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-         &nbsp;numbers long
-      </p>
-
-      <p> 
-        I want the timer to be set to:&nbsp; 
-        <select value={timerLength} onChange={e => setTimerLength(Number(e.target.value))}>
-          {Array.from({ length: 16 }, (_, i) => i + 5).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select> 
-        &nbsp;seconds
-      </p>
-    <hr />
-      <p>Click "Generate Sequence" to start. A random sequence of numbers will be shown. Memorize this sequence, as it will disappear after 10 seconds. You will then have to enter the sequence in reverse.</p>
+      <hr />
+      <div className="input-text">
+        <p> Let's start by configuring the game :</p> 
+        <label>
+          - Your name:&nbsp; 
+          <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        </label>
+        <p>- I want the sequence to be :&nbsp;
+          <select value={numDigits} onChange={e => setNumDigits(Number(e.target.value))}>
+            {Array.from({ length: 16 }, (_, i) => i + 5).map(num => (
+              <option key={num} value={num}>{num}</option>
+            ))}
+          </select>
+          &nbsp;numbers long
+        </p>
+        <p> 
+          - I want the timer to be set to:&nbsp; 
+          <select value={timerLength} onChange={e => setTimerLength(Number(e.target.value))}>
+            {Array.from({ length: 16 }, (_, i) => i + 5).map(num => (
+              <option key={num} value={num}>{num}</option>
+            ))}
+          </select> 
+          &nbsp;seconds
+        </p>
+      </div>
+      <hr />
+      <div className="explain-text">
+        <p>Click "Generate Sequence" to start.</p> 
+        <p>A random sequence of numbers will be shown. Memorize this sequence, as it will disappear after 10 seconds. You will then have to enter the sequence in reverse.</p>
+      </div>
       <button onClick={generateSequence}>Generate Sequence</button>
       <p>Time left: {countdown}</p>
-      <div className="sequence-container">
+      <div className="sequence-container" ref={sequenceRef}>
         {digitSequence.map((digit, i) => (
           <div key={i} className="sequence-item">{digit}</div>
         ))}
@@ -163,12 +175,9 @@ useEffect(() => {
             </div>
             <div></div>
           </div>
-          
           <button type="submit" onClick={checkSequence} ref={submitRef}>Submit</button>
-          </>
-          
+        </>
       )}
-      
       {showModal && (
         <Modal isOpen={showModal} onRequestClose={() => setShowModal(false)}>
           <h2>Résultats</h2>
